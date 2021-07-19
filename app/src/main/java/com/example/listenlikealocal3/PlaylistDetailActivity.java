93a1d164a9d8bebe7a2d67f5339ff0eceeb4eafa
Briya Philip <briya@fb.com>
2021-07-19 15:02:11 -0500

    Parsed song from playlist

--- app/src/main/java/com/example/listenlikealocal3/PlaylistDetailActivity.java
+++ app/src/main/java/com/example/listenlikealocal3/PlaylistDetailActivity.java
@@ -15,6 +15,7 @@ import com.example.listenlikealocal3.Model.Song;
 import com.example.listenlikealocal3.Services.PlaylistItems;
 import com.example.listenlikealocal3.Services.Playlists;
 import com.example.listenlikealocal3.Services.Songs;
+import com.example.listenlikealocal3.databinding.ActivityPlaylistDetailsBinding;
 import com.example.listenlikealocal3.databinding.ActivityPlaylistsBinding;
 
 import java.util.ArrayList;
@@ -23,27 +24,33 @@ public class PlaylistDetailActivity extends AppCompatActivity {
     private TextView playlistName;
     private TextView songName;
     private Song song;
+    private ActivityPlaylistDetailsBinding binding;
 
     private SongService songService;
     private ArrayList<Song> playlistItems;
+    private ArrayList<String> playlistItemList;
     private Playlist playlist_id;
+
+    private ArrayAdapter arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playlist_details);
-
         songService = new SongService(getApplicationContext());
-        playlistName = (TextView) findViewById(R.id.name_txtView_name);
-        songName = (TextView) findViewById(R.id.Songs);
-        playlist_id = new Playlist(getApplicationContext());
+
 
         SharedPreferences sharedPreferences = this.getSharedPreferences("SPOTIFY", 0);
         String name = sharedPreferences.getString("name", null);
-        playlistName.setText(name);
 
+        playlistName = (TextView) findViewById(R.id.playlist_name);
+        songName = (TextView) findViewById(R.id.song_name);
+        playlist_id = new Playlist(getApplicationContext());
+        playlistName.setText(name);
         getTracks();
+
     }
+
 
     private void getTracks() {
         songService.getPlaylistItems(() -> {
